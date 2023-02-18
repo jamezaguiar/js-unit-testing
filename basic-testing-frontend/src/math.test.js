3e7829fa1a0c2f7fe368b14f01ed5dcf906ea978
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2023-02-18 19:26:02 -0300

    AAA

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -2,6 +2,10 @@ import { expect, it } from 'vitest';
 import { add } from './math';
 
 it('should summarize all number values in an array', () => {
-  const result = add([1, 2, 3]);
-  expect(result).toBe(6);
+  const numbers = [1, 2, 3];
+  const expectedResult = numbers.reduce((prev, curr) => (prev += curr), 0);
+
+  const result = add(numbers);
+
+  expect(result).toBe(expectedResult);
 });
