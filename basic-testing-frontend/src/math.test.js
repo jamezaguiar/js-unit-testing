c44be662a875377ff032c71188710d9e7119abc1
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2023-02-20 11:01:09 -0300

    defining behaviors & fixing errors in your code

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -2,10 +2,27 @@ import { expect, it } from 'vitest';
 import { add } from './math';
 
 it('should summarize all number values in an array', () => {
-  const numbers = [1, 2, 3];
-  const expectedResult = numbers.reduce((prev, curr) => (prev += curr), 0);
+  const inputs = [1, 2, 3];
+  const expectedResult = inputs.reduce((prev, curr) => prev + curr, 0);
 
-  const result = add(numbers);
+  const result = add(inputs);
+
+  expect(result).toBe(expectedResult);
+});
+
+it('should yield NaN if a least one invalid number is provided', () => {
+  const inputs = ['invalid', 1];
+
+  const result = add(inputs);
+
+  expect(result).toBeNaN();
+});
+
+it('should yield a correct sum if an array of numeric string values is provided', () => {
+  const inputs = ['1', '2'];
+  const expectedResult = inputs.reduce((prev, curr) => +prev + +curr, 0);
+
+  const result = add(inputs);
 
   expect(result).toBe(expectedResult);
 });
