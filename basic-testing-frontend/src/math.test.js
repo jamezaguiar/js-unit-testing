64a0e433cce5ea722949dd76d458742c23119ac4
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2023-02-20 11:11:19 -0300

    testing for errors

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -26,3 +26,17 @@ it('should yield a correct sum if an array of numeric string values is provided'
 
   expect(result).toBe(expectedResult);
 });
+
+it('should yield 0 if an empty array is provided', () => {
+  const inputs = [];
+
+  const result = add(inputs);
+
+  expect(result).toBe(0);
+});
+
+it('should throw an error if no value is passed into the function', () => {
+  const resultFn = () => add();
+
+  expect(resultFn).toThrow();
+});
