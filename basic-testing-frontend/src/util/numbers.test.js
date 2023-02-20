49eaa6b2cd8c7971ff6e24e744ab790e157403bd
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2023-02-20 12:08:21 -0300

    multiple assertions

--- basic-testing-frontend/src/util/numbers.test.js
+++ basic-testing-frontend/src/util/numbers.test.js
@@ -6,13 +6,17 @@ it('should transform a string number to a number of type number', () => {
 
   const result = transformToNumber(input);
 
+  expect(result).toBe(1);
   expect(result).toBeTypeOf('number');
 });
 
 it('should yield NaN if the argument is not a numeric string', () => {
   const input = 'invalid';
+  const input2 = {};
 
   const result = transformToNumber(input);
+  const result2 = transformToNumber(input2);
 
   expect(result).toBeNaN();
+  expect(result2).toBeNaN();
 });
