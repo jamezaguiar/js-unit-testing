781e8197db483c9f433128fd19312d9f48a006e2
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2023-03-10 09:28:48 -0300

    testing asynchronous code with callbacks

---
+++ async-code-project/async/async-example.test.js
@@ -0,0 +1,15 @@
+import { expect, it } from 'vitest';
+import { generateToken } from './async-example';
+
+it('should generate a token value', done => {
+  const testUserEmail = 'test@test.com';
+
+  generateToken(testUserEmail, (err, token) => {
+    try {
+      expect(token).toBeDefined();
+      done();
+    } catch (err) {
+      done(err);
+    }
+  });
+});
